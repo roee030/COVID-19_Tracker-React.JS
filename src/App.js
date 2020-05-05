6afd3f66ee4cd75c6aef1a01627e7e62c90a6589
roee030 <roee.angel93@gmail.com>
2020-05-05 14:02:23 +0300

    add api for the covid19 system

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 
+import {Cards, Chart , CountryPicker} from './components';
+import styles from './App.module.css';
+import {fetchData} from './api/';
+
 class App extends React.Component {
+    async componentDidMount (){
+        const response = await fetchData()
+        
+        
+    }
     render() {
         return (
-            <h1>
-                Hello from App
-            </h1>
+            <div className={styles.container}>
+                <Cards/>
+                <CountryPicker/>
+                <Chart/>
+            </div>
         )
     }
 }
