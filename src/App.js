4359b45dd4ff74bf349b9c35f826eb9cbd2b24d8
roee030 <roee.angel93@gmail.com>
2020-05-06 18:12:45 +0300

    final refactor

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import React from 'react';
 import { Cards, CountryPicker, Chart } from './components';
 import { fetchData } from './api/';
 import styles from './App.module.css';
-
+import BackVideo from './images/1111.mp4'
+import MidBackVideo from './images/mid.mp4';
 import image from './images/image.png';
 
 class App extends React.Component {
@@ -28,12 +29,23 @@ class App extends React.Component {
     const { data, country } = this.state;
 
     return (
+      
+      <>
+      
       <div className={styles.container}>
+      
         <img className={styles.image} src={image} alt="COVID-19" />
         <Cards data={data} />
         <CountryPicker handleCountryChange={this.handleCountryChange} />
         <Chart data={data} country={country} /> 
       </div>
+      <div className="styles.bg-video">
+        <video className='styles.bg-video__content'  autoPlay loop muted>
+          <source src={BackVideo} type='video/mp4' media="all and (max-width: 900px)"/>
+          <source src={MidBackVideo} type="video/mp4" media="all and (max-width: 480px)"/> 
+        </video>
+      </div>
+      </>
     );
   }
 }
